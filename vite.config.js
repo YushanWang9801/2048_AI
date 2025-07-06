0aeafc20160be37cdc823ed3ee6ed623e03c50a7
yushanwang9801 <yushanwang0816@gmail.com>
2025-07-06 22:11:44 +0800

    update package

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  base: '/2048_AI/', // 替换为你的仓库名
+  plugins: [vue()] // 或 vue()
+});
